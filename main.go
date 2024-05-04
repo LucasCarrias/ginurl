826bbd9784b6083c665c32220ba412e7d4c6d59b
Lucas Carrias <lucascarrias@outlook.com>
2024-05-04 16:28:03 -0300

    create shorten func

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+const BaseUrl = "localhost:8080/"
+
+type ShortenedUrl struct {
+	original string
+	url string
+}
+
+type FormData struct {
+	Url string `form:"url"`
+}
 
 func main() {
 	router := gin.Default()
@@ -21,15 +33,27 @@ func rootHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
 
-type FormData struct {
-	Url string `form:"url"`
-}
-
 func shortenHandler(c *gin.Context) {
 	data := &FormData{}
-
 	c.Bind(data)
+
+	shortenedUrl := BuildShortenedUrl(data.Url)
+
 	c.HTML(http.StatusOK, "url.html", gin.H{
-		"url": data.Url,
+		"url": shortenedUrl.url,
 	})
+}
+
+func BuildShortenedUrl(url string) ShortenedUrl {
+	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var result []byte
+
+	for i := 0; i < 5; i++ {
+		result = append(result, chars[rand.Intn(len(chars))])
+	}
+
+	return ShortenedUrl{
+		original: url,
+		url: BaseUrl + string(result),
+	}
 }
